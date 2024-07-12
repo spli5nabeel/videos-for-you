14f8c468ad8772cdfc29e84c9678e177faae0b8f
Nabeel Ahmed <nabeel@Nabeels-Air.lan>
2024-07-12 12:57:28 +0100

    base code

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, Welcome to videos for you !</h1>
+       <h2>Happy Viewing!!</h2>
+    </div>
+  );
+}
+
+export default App;
